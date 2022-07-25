ffdf268813af9ddb1d58d67ddbd5242678ceccaf
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-25 08:14:32 -0500

    observer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { Observable, Observer } from 'rxjs';
 
-//  anterior forma const obs$ = Observable.create();
 
 const observer: Observer<any> = {
     next: value => console.log( 'Siguiente [Next]: ', value ),
@@ -8,29 +7,3 @@ const observer: Observer<any> = {
     complete: () => console.info('Completado [Obs]')
 }
 
-const obs$ = new Observable<string>( subs => {
-
-    subs.next('Hola');
-    subs.next('Mundo');
-
-    subs.next('Hola');
-    subs.next('Mundo');
-
-    // Forzar un error
-    //  error const a = undefined;
-    // error a.nombre = 'Jesus;
-
-    subs.complete();
-
-    subs.next('Hola');
-    subs.next('Mundo');
-
-});
-
-obs$.subscribe( observer );
-
-// code obs$.subscribe( valor =>  
-// code     console.log('Next: ', valor),
-// code     error => console.warn('Error: ', error),
-// code     () => console.info('Completado')
-// code );
