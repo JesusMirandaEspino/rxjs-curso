00759b3a99f7f502279b052770d1fc9703079511
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-25 08:30:47 -0500

    unsuscribe

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,25 @@ const observer: Observer<any> = {
     complete: () => console.info('Completado [Obs]')
 }
 
+const intervalos$ = new Observable<number>( suscriber => {
+    let count = 0;
+const intervalo =  setInterval( () => {
+                        count++;
+                        suscriber.next( count );
+                        console.log(count);
+                    }, 2500);
+    return () => {
+        clearInterval( intervalo );
+        console.log('Intervalo');
+    };
+});
+
+const subs1 =intervalos$.subscribe( num => console.log( num ) );
+const subs2 =intervalos$.subscribe( num => console.log( num ) );
+const subs3 =intervalos$.subscribe( num => console.log( num ) );
+
+setTimeout( () => {
+    subs1.unsubscribe();
+    subs2.unsubscribe();
+    subs3.unsubscribe();
+}, 7500);
