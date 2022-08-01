d58e7846a2ed32dd5d0e16cfa98ea4dc0b75b610
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-08-01 08:52:23 -0500

    map part 2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import { range } from 'rxjs';
+import { fromEvent, range } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 range(1,5).pipe( 
-    map<number, number>( val => val * 10 )
+    map<number, string>( val => (val * 10).toString() )
 ).subscribe( console.log );
+
+
+const keyup$ = fromEvent<KeyboardEvent>( document, 'keyup' );
+
+const keyUpCode = keyup$.pipe( map( event => event.code ) );
+
+
+keyUpCode.subscribe( val => console.log( 'map', val ) );
