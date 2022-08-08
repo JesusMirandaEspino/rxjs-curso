590763d54d9425f7af0fe77d6ba8960bf2828844
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-08-08 08:19:05 -0500

    firts part 2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { fromEvent } from "rxjs";
-import { take, first } from "rxjs/operators";
+import { take, first, tap } from "rxjs/operators";
 
 const click$ = fromEvent<MouseEvent>( document, 'click' );
 
 
 click$.pipe(
-    first()
+    tap( console.log ),
+    first<MouseEvent>( event => event.clientY >= 150 )
     ).subscribe({
         next: val => console.log( val ),
         complete: () => console.info('Completado [Obs]')
