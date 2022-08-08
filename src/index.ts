facfb581c04865c209a01d41949aab6cd9a6202e
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-08-08 08:04:51 -0500

    take

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,12 @@
-import { from } from "rxjs";
-import {  reduce, scan, map } from "rxjs/operators";
+import { of } from "rxjs";
+import { take, tap } from "rxjs/operators";
 
-const numbers = [ 1,2,3,4,5,6,7,8,9 ];
+const numeros$ = of(1,2,3,4,5);
 
-const totalAcumulador = ( acc: number, cur: number ) =>  acc + cur;
-
-from( numbers )
-    .pipe( reduce( totalAcumulador, 0 ) )
-    .subscribe( console.log );
-
-
-from( numbers )
-    .pipe( scan( totalAcumulador, 0 ) )
-    .subscribe( console.log );
-
-
-// Redux
-interface Usuario {
-    id?: string;
-    autenticado?: boolean;
-    token?: string;
-    edad?: number;
-}
-
-const user: Usuario[] = [
-    { id: 'fher', autenticado: false, token: null },
-    { id: 'fher', autenticado: true, token: 'ABC' },
-    { id: 'fher', autenticado: true, token: 'ABC123' },
-];
-
-const state$ = from( user ).pipe(
-    scan<Usuario, Usuario>( (acc, cur) => {
-        return { ...acc, ...cur }
-    }, { edad: 33 })
-);
-
-const id$ = state$.pipe(
-    map( state => state.id )
-);
-
-id$.subscribe( console.log );
-
+numeros$.pipe(
+    tap( console.log ),
+    take( 3 )
+    ).subscribe({ 
+        next: val => console.log( val ),
+        complete: () => console.info('Completado [Obs]')
+    });
