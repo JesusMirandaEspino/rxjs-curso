3f17c58adaccefa39e16e45b748d0e53fb950b4a
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-08-10 08:36:26 -0500

    takeUntil part 2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { fromEvent, interval } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+
 
 const boton = document.createElement('button');
 
@@ -6,5 +8,15 @@ boton.innerHTML = 'Detener Timer';
 
 document.querySelector('body').append(boton);
 
-const counter = interval(1000);
-const clickBoton = fromEvent( boton, 'click' );
+const counter$ = interval(1000);
+const clickBoton$ = fromEvent( boton, 'click' );
+
+
+counter$.pipe(
+            takeUntil(clickBoton$)
+            ).subscribe({
+                next: val => console.log( val ),
+                complete: () => console.info('Completado [Obs]')
+        });
+
+
