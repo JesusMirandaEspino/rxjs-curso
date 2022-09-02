63001b12f040f84574f4191d8afdedd63679037d
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-09-02 14:49:07 -0500

    catchError

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { catchError, pluck } from 'rxjs/operators';
+import { ajax, AjaxError } from 'rxjs/ajax'
+import { of } from 'rxjs';
+
 const url = 'https://api.github.com/users?per_page=5';
 
 const manejaErrores = (response: Response) => {
@@ -8,10 +12,23 @@ const manejaErrores = (response: Response) => {
     return response;
 }
 
+const atrapaError = (err: AjaxError) => {
+            console.warn('Error ', err);
+                return of([]);
+            }
+
 const fecthPromesa = fetch(url);
 
+
+/* code promise
 fecthPromesa
     .then( manejaErrores )
     .then((resp) => {   resp.json()
     .then((data) => {console.log(data);});
-}).catch( error => {console.warn('Error ', error)});
+}).catch( error => {console.warn('Error ', error)});*/
+
+
+ajax(url).pipe(
+    pluck( 'response' ),
+    catchError( atrapaError )
+    ).subscribe( user => {console.log(user)});
