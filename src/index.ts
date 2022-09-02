33d965cee661d641e9fdd11bbaa62eb5d5cdfc2c
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-09-02 14:29:41 -0500

    response

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 const url = 'https://api.github.com/users?per_page=5';
 
+const manejaErrores = (response: Response) => {
+    if(!response.ok){
+        throw new Error(response.statusText);
+    }
+
+    return response;
+}
+
 const fecthPromesa = fetch(url);
 
-fecthPromesa.then((resp) => {
-    resp.json().then((data) => {console.log(data);});
+fecthPromesa
+    .then( manejaErrores )
+    .then((resp) => {   resp.json()
+    .then((data) => {console.log(data);});
 }).catch( error => {console.warn('Error ', error)});
