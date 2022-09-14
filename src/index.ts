d68be0eb064d3ccd2413afdcf068e2405a4f1ccb
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-09-14 14:54:22 -0500

    get

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const body = document.querySelector('body');
+
+const textInput = document.createElement('input');
+const orderList = document.createElement('ol');
+
+body.append( textInput, orderList );
