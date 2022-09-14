8fbeea6c2dcc384cd327da4485e3b362e3b9228b
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-09-14 14:59:47 -0500

    merge

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+import { fromEvent } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { debounceTime, map } from "rxjs/operators";
+
 const body = document.querySelector('body');
 
 const textInput = document.createElement('input');
 const orderList = document.createElement('ol');
 
 body.append( textInput, orderList );
+
+
+const input$ = fromEvent<KeyboardEvent>( textInput, 'keyup' );  
+
+input$.pipe( 
+        debounceTime(500),
+        map( event =>{
+            const texto = event.target['value'];
+            return ajax.getJSON( `https://api.github.com/users/${texto}` )
+        })
+    ).subscribe( resp =>{ resp.subscribe( console.log) } );
